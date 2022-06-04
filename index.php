7e4da20c58607b7e32261cb4d1fa44214b0a4b60
Noah Boyd <noahboyd1007@icloud.com>
2022-06-04 18:06:08 -0400

    First commit for Skate Website

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skate</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
+    <div class="login-container">
+        <div class="inner-login-container">
+            <div class="login-logo-container">
+
+            </div>
+
+            <span class="login-brand-name">Brand Name</span>
+
+            <input class="login-text-field" placeholder="Username">
+
+            <input type="password" class="login-text-field" placeholder="Password">
+
+            <input type="submit" class="login-submit-button" value="Login">
+
+            <span class="login-small-text">Forgot your password?</span>
+
+            <span class="login-small-text login-small-text-signup">Dont have an account?</span>
+
+            <input type="button" class="login-submit-button" value="Signup">
+
+            <div class="login-footer">Socials go here</div>
+
+        </div>
+    </div>
+</body>
+
+</html>
