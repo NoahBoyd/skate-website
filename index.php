b9282ee399d711f90e8dfacd5c25a01903557875
Noah Boyd <nboyd@stackpole.com>
2022-06-08 08:00:24 -0400

    Added signupUser, and changed head

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php include "pages/head.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skate</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="script/index.js" defer></script>
+</head>
+
 <body>
     <div class="login-container">
         <div class="inner-login-container">
---
+++ pages/signupUser.php
@@ -0,0 +1,21 @@
+<?php include "head.php" ?>
+<body>
+    <div class="login-container">
+        <div class="inner-login-container">
+            <div class="login-logo-container">
+                <img class="logo" src="images/tempLogo.svg" alt="">
+            </div>
+
+            <span class="login-brand-name">Create Account</span>
+
+            <div id="signup" class="login-button">
+                    Signup
+            </div>
+
+            <div class="login-footer">Socials go here</div>
+
+        </div>
+    </div>
+</body>
+
+</html>
