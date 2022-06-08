112320638a38650ea49f46680d604f10f27a6349
Noah Boyd <nboyd@stackpole.com>
2022-06-08 12:21:47 -0400

    signup page layout version 1

--- pages/signupUser.php
+++ pages/signupUser.php
@@ -1,15 +1,33 @@
 <?php include "head.php" ?>
+<script src="../script/signup.js"></script>
 <body>
     <div class="login-container">
         <div class="inner-login-container">
+            <div class="signup-back-button-container">
+                <button class="signup-back-button">
+                    <img class="back-button-image" src="../images/back-arrow.svg" alt="">
+                </button>
+            </div>
             <div class="login-logo-container">
-                <img class="logo" src="images/tempLogo.svg" alt="">
+                <img class="logo" src="../images/tempLogo.svg" alt="">
             </div>
 
             <span class="login-brand-name">Create Account</span>
 
-            <div id="signup" class="login-button">
-                    Signup
+            <input type="text" class="wide-text-field" placeholder="Email">
+
+            <input type="text" class="wide-text-field" placeholder="Username">
+
+            <input type="password" class="wide-text-field" placeholder="Password">
+
+            <input type="password" class="wide-text-field" placeholder="Confirm Password">
+
+            <span class="error-text">Error</span>
+
+            <div id="signup" class="wide-button">
+                <div class="wide-button-inner">
+                    Sign up
+                </div>
             </div>
 
             <div class="login-footer">Socials go here</div>
