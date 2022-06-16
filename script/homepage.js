e8b5264c6b97cb6b2bd87d27523d42a4d3a0063e
Noah Boyd <nboyd@stackpole.com>
2022-06-16 12:58:39 -0400

    added animation to hamburger menu

---
+++ script/homepage.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', () => {
+    console.log('hello from homepage.js')
+
+    // variables
+    let menuOpen = false;
+
+
+    // fields
+    let menuButton = $('.hamburger-menu');
+
+
+    // functions
+
+
+    // click events
+
+    // menu button toggle open
+    $(menuButton).on('click', () => {
+        console.log('open menu');
+        if (!menuOpen) {
+            $(menuButton).addClass('open');
+            menuOpen = true;
+        } else {
+            $(menuButton).removeClass('open');
+            menuOpen = false;
+        }
+    });
+
+})
